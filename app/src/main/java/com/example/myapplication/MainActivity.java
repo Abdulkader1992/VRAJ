dc3cfd97ca69b44aa66b38e5d313824734a93a9d
Abdulkader1992 <ebdo93@gmail.com>
2019-10-01 15:17:52 +0200

    My first Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+        TextView title;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        float size;
+        size = new Button(this).getTextSize();
+        Toast.makeText(this, "Textgröße" + size, Toast.LENGTH_LONG).show();
+        title = (TextView)this.findViewById(R.id.textView_title);
+        Button red = (Button)this.findViewById(R.id.buttonRed);
+        Button yellow = (Button)this.findViewById(R.id.buttonYellow);
+        Button blue = (Button)this.findViewById(R.id.buttonBlue);
+
+        red.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        blue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.buttonRed:
+                title.setTextColor(Color.RED);
+                break;
+            case R.id.buttonYellow:
+                title.setTextColor(Color.YELLOW);
+                break;
+            case R.id.buttonBlue:
+                title.setTextColor(Color.BLUE);
+                break;
+        }
     }
 }
